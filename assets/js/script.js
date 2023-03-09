127012425a647ec9cde5c4c21219457163dd1b3f
Green Camper <stefan.palacios@gmail.com>
2023-03-08 21:34:58 -0600

    Added API call function for Netflix search

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,27 @@
 
 */
 
+//Jquery wrapper
+$(function(){
+  //Jquery handler function
+  $(document).ready(function(){
+
+    //Function code provided by API with parameters included
+    //Parameters included: 20 results PP, US country limit
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '0d055c8f5cmsh911d3bd3fd0acf3p1394b3jsn4a88133a76fd',
+        'X-RapidAPI-Host': 'unogs-unogs-v1.p.rapidapi.com'
+      }
+    };
+    
+    fetch('https://unogs-unogs-v1.p.rapidapi.com/search/titles?limit=20&order_by=date&country_list=78', options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
+      
+  });
+
+});
+
